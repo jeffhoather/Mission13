69d0412e4d70c74fc723b6d2efeeea348bf336e3
Jeff Hoather <jhoather@byu.edu>
2022-03-28 14:20:24 -0600

    Add project files.

---
+++ Mission13/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission13.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BowlersDbContext _context { get; set; }
+ 
+        public HomeController(BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Bowlers.ToList();
+
+            return View(blah);
+        }
+
+
+
+        
+    }
+}
